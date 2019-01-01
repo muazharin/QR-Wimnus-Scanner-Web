4bc72f0bf030645a0d07a6007aafa4a7bf94a2a6
root <alfanmuazharin@gmail.com>
2019-01-02 07:15:51 +0800

    file autentikasi android dan web admin

---
+++ pesertaHadir.php
@@ -0,0 +1,31 @@
+<?php
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		
+		//Import File Koneksi database
+		require_once('koneksi.php');
+		
+		//Mendapatkan Nilai Variable
+		$id = $_POST['idPes'];
+		$sql1 =  "SELECT * FROM data_peserta WHERE kehadiran = 'hadir' AND idPes = $id;";
+		$hasil=mysqli_query($con, $sql1);
+		$jml_data=mysqli_num_rows($hasil);
+		if ($jml_data<=0) {
+			//Pembuatan Syntax SQL
+			$sql =  "UPDATE data_peserta SET kehadiran = 'hadir' WHERE idPes = $id;";
+			//$sql = "INSERT INTO tb_pegawai (nama,posisi,gajih) VALUES ('$name','$desg','$sal')";
+			
+			//Eksekusi Query database
+			if(mysqli_query($con,$sql)){
+				echo 'Berhasil Menambahkan Peserta';
+			}else{
+				echo 'QR Code tidak dikenal! Silahkan hubungi admin~';
+			}
+		
+		} else {
+			echo 'Peserta sudah terdaftar sebelumnya!';
+		}
+		
+		
+		mysqli_close($con);
+	}
+?>
